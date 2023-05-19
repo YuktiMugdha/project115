f89458095347fb8fe1a0aa3ea78280aba45eb360
YuktiMugdha <88884410+YuktiMugdha@users.noreply.github.com>
2023-05-19 19:43:17 +0530

    Add files via upload

---
+++ C115.py
@@ -0,0 +1,5 @@
+import os
+source= "C:/Users/hp/OneDrive/Desktop/python/class 111"
+dest="C:/Users/hp/OneDrive/Desktop/python/class 115"
+os.rename(source,dest)
+print("Ur file has been renamed succesfully")
